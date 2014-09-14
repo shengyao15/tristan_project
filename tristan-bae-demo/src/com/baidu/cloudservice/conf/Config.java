a908928793cce4ab4bdc707260051242ba5ef99a
shengyao15 <shengyao15@126.com>
2014-09-14 19:01:16 +0800

    update

---
+++ tristan-bae-demo/src/com/baidu/cloudservice/conf/Config.java
@@ -0,0 +1,37 @@
+package com.baidu.cloudservice.conf;
+
+public class Config{
+
+  	/*** API KEY AND ACCESS KEY ***/
+	public static String USER = "M7VYjUNhrUxRzAK60lZezkAI";
+	public static String PWD = "W2lTKtmvxRBFlqUn0F60yRQiiLnh73La";
+
+  	/***MySQL相关配置***/
+	public static String MYSQLNAME = "x";
+	public static String MYSQLHOST = "sqld.duapp.com";
+	public static String MYSQLPORT = "4050";
+
+  	/***Mongo相关配置***/
+	public static String MONGONAME = "WClyaICXIUhGXauwZrkR";
+	public static String MONGOHOST = "mongo.duapp.com";
+	public static String MONGOPORT = "8908";
+
+  	/***Redis相关配置***/
+	public static String REDISNAME = "gJaUNMsUWIpWqeyNMQJW";
+	public static String REDISHOST = "redis.duapp.com";
+	public static String REDISPORT = "80";
+  	
+  	/***Cache相关配置***/
+	public static String CACHEID = "uukDomoWJNEcxSQbTujA";
+	public static String CACHEHOST = "cache.duapp.com";
+	public static String CACHEPORT = "20243";
+
+	/***Image配置***/
+  	public static String IMAGEHOST="image.duapp.com";
+
+	/***BCS配置***/
+  	public static String BCSHOST="bcs.duapp.com";
+  	public static String BUCKET="xxxx";
+  
+
+}
